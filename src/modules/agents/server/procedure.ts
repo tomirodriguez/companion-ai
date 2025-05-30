eaf5f18fe67cc35604f28baa31000806450a10a6
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2025-05-30 09:46:21 -0300

    ui: add responsive dialogs

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -1,7 +1,6 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
 	getMany: baseProcedure.query(async () => {
