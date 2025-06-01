7a628cb13d3fa6a3febcf4e781bacd3cc9ec530b
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2025-06-01 13:40:45 -0300

    add transcriptions

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,9 +1,13 @@
 import { db } from "@/db";
 import { agents, meetings } from "@/db/schema";
+import { inngest } from "@/inngest/client";
 import { streamVideo } from "@/lib/stream-video";
 import type {
+	CallEndedEvent,
+	CallRecordingReadyEvent,
 	CallSessionParticipantLeftEvent,
 	CallSessionStartedEvent,
+	CallTranscriptionReadyEvent,
 } from "@stream-io/node-sdk";
 import { and, eq } from "drizzle-orm";
 import { type NextRequest, NextResponse } from "next/server";
@@ -103,6 +107,69 @@ export async function POST(req: NextRequest) {
 
 		const call = streamVideo.video.call("default", meetingId);
 		await call.end();
+	} else if (eventType === "call.session_ended") {
+		const event = payload as CallEndedEvent;
+		const meetingId = event.call.custom?.meetingId;
+
+		if (!meetingId) {
+			return NextResponse.json({ error: "Missing meetingId" }, { status: 404 });
+		}
+
+		await db
+			.update(meetings)
+			.set({
+				status: "processing",
+				endedAt: new Date(),
+			})
+			.where(and(eq(meetings.id, meetingId), eq(meetings.status, "active")));
+	} else if (eventType === "call.transcription_ready") {
+		const event = payload as CallTranscriptionReadyEvent;
+		const meetingId = event.call_cid.split(":")[1];
+
+		if (!meetingId) {
+			return NextResponse.json(
+				{ error: "Meeting Id not found" },
+				{ status: 404 },
+			);
+		}
+
+		const [updatedMeeting] = await db
+			.update(meetings)
+			.set({
+				transcriptUrl: event.call_transcription.url,
+			})
+			.where(eq(meetings.id, meetingId))
+			.returning();
+
+		if (!updatedMeeting) {
+			return NextResponse.json({ error: "Meeting not found" }, { status: 404 });
+		}
+
+		await inngest.send({
+			name: "meetings/processing",
+			data: {
+				meetingId: updatedMeeting.id,
+				transcriptUrl: updatedMeeting.transcriptUrl,
+			},
+		});
+	} else if (eventType === "call.recording_ready") {
+		const event = payload as CallRecordingReadyEvent;
+		const meetingId = event.call_cid.split(":")[1];
+
+		if (!meetingId) {
+			return NextResponse.json(
+				{ error: "Meeting Id not found" },
+				{ status: 404 },
+			);
+		}
+
+		await db
+			.update(meetings)
+			.set({
+				recordingUrl: event.call_recording.url,
+			})
+			.where(eq(meetings.id, meetingId))
+			.returning();
 	}
 
 	return NextResponse.json({ status: "ok" });
--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -5,3 +5,11 @@ export type MeetingGetMany =
 	inferRouterOutputs<AppRouter>["meetings"]["getMany"]["items"];
 
 export type MeetingGetOne = inferRouterOutputs<AppRouter>["meetings"]["getOne"];
+
+export type StreamTranscriptItem = {
+	speaker_id: string;
+	type: string;
+	text: string;
+	start_ts: number;
+	stop_ts: number;
+};
